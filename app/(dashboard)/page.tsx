b897989b5cd46329517469d0469201161c1ac654
Niksan Karkee <niksankarkee@Niksans-MacBook-Pro.local>
2024-05-13 20:36:12 +0900

    accounts post

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+
 export default function Home() {
-  return <div className=''>Home</div>;
+  const { onOpen } = useNewAccount();
+  return (
+    <div className=''>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { QueryProviders } from '@/providers/query-provider';
+import SheetProvider from '@/providers/sheet-provider';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +22,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
-          <QueryProviders>{children}</QueryProviders>
+          <QueryProviders>
+            <SheetProvider />
+            <Toaster />
+            {children}
+          </QueryProviders>
         </body>
       </html>
     </ClerkProvider>
